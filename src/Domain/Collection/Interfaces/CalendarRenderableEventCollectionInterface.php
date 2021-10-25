fc68886770ccd7c018a65eec888496e59c8f6e5d
Sam Baynham <sam@fifthgate.net>
2021-10-25 12:22:07 +0100

    Removing hard dependency on DomainEntityCollections

--- src/Domain/Collection/Interfaces/CalendarRenderableEventCollectionInterface.php
+++ src/Domain/Collection/Interfaces/CalendarRenderableEventCollectionInterface.php
@@ -5,7 +5,7 @@ namespace Fifthgate\CalendarGenerator\Domain\Collection\Interfaces;
 use \DateTimeInterface;
 use Fifthgate\Objectivity\Core\Domain\Collection\Interfaces\DomainEntityCollectionInterface;
 
-interface CalendarRenderableEventCollectionInterface extends DomainEntityCollectionInterface
+interface CalendarRenderableEventCollectionInterface
 {
     public function filterBetweenDates(DateTimeInterface $start, DateTimeInterface $end, bool $includeEndPoints = true, bool $allowOverlaps = true) : ? CalendarRenderableEventCollectionInterface;
 }
